ca4d5f277b95dbdc53bc0003c5ad0b772b9ccd91
Elisabet Lövkvist <elisabet@squeeee.se>
2013-07-19 23:52:25 +0200

    Added ability to specify length of stripe sequence.

--- heelcount.py
+++ heelcount.py
@@ -1,13 +1,24 @@
-rnd = 64 #at the beginning of the heel there are 64 sts total
-sts = rnd*3 #these are worked 3 rounds before decreases start
 
-while (rnd > 24):
+
+rnd = int(input('How many sts do you start with? '))
+end = int(input('How many sts do you end with? '))
+
+sts = rnd*3 #there are worked 3 rounds before decreases start
+
+
+
+while (rnd > end):
   rnd-=4 #4 sts decrease every other round
   sts+=rnd*2 #...so there are 2 rounds worked with this stitch count
-  print "Stitch count is ", rnd
-  print "No sts worked total: ", sts
+#  print "Stitch count is ", rnd
+#  print "No sts worked total: ", sts
 
-stripe = 6*60 #the self striping yarn is 6 rows over 60 sts => 360 sts/stripe
+stripel = int(input('How many rounds is the stripe sequence? '))
+stripew = int(input('Over how many sts? '))
+
+stripe = stripel*stripew #number of sts in a stripe sequence
+
 stripes = sts/stripe #number of stripes needed for the heel
 
-print stripes, " whole stripes needed for heel, plus ", sts%stripe, " sts."     
+
+print(stripes, " stripe sequence(s) needed for heel.")
